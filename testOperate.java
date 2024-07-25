29a56d39eee17c22095c26df60cf5f121172821a
thittiya007 <152352137+thittiya007@users.noreply.github.com>
2024-07-25 14:40:53 +0700

    Create testOperate.java

---
+++ testOperate.java
@@ -0,0 +1,10 @@
+public class testOperate {
+   public static void main(String[] args) {
+    int x, y;
+    x = 5 + 2 * 4;
+    y = (5 + 2) *4;
+    System.out.println("x ="+x);
+    System.out.println("y ="+y);
+   }
+
+}
