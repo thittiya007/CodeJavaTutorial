cc5855419a9b3c2994c87d9eff4d7af775e92727
thittiya007 <152352137+thittiya007@users.noreply.github.com>
2024-07-25 14:40:49 +0700

    Create tesNum.java

---
+++ tesNum.java
@@ -0,0 +1,13 @@
+public class tesNum {
+    public static void main(String args[]) {
+          int x, y; //ประก่ษตัวแปร
+          x = 10;
+          y = x+1;
+          System.out.println("x = " +x+"y = "+y);
+          y = y++ ;
+          System.out.println("x = " +x+"y = "+y);
+          y = ++y ;
+          System.out.println("x = " +x+"y = "+y);
+   }
+}   
+
