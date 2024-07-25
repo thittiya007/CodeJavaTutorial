2f227565d88bb1f57e26482727ee6e8f5174dade
thittiya007 <152352137+thittiya007@users.noreply.github.com>
2024-07-25 14:40:31 +0700

    Create exNum.java

---
+++ exNum.java
@@ -0,0 +1,9 @@
+public class exNum {
+    public static void main(String[] args) {
+        int x,y,total;
+        x = 62;
+        y = 99;
+        total = x+y;
+        System.out.println("total=" +total);
+    }
+}
