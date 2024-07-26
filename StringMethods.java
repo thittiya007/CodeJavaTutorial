6a2d5b45124ae2aa88b7977e7f4b09d932641f89
thittiya007 <152352137+thittiya007@users.noreply.github.com>
2024-07-26 14:50:12 +0700

    Create StringMethods.java

---
+++ StringMethods.java
@@ -0,0 +1,14 @@
+public class StringMethods {
+    public static void main(String[] args) {
+        String message = "Java is Grdat Fun";
+
+        String upper = message.toUpperCase();
+        String lower = message.toUpperCase();
+        char letter = message.charAt(3);
+        int StringSize = message.length();
+        System.out.println(upper);
+        System.out.println(lower);
+        System.out.println(letter);
+        System.out.println(StringSize);
+    }
+}
