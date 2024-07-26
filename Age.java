ad001e121d57750da17054349536d9bf26d5f4d8
thittiya007 <152352137+thittiya007@users.noreply.github.com>
2024-07-26 15:00:44 +0700

    Update Age.java

--- Age.java
+++ Age.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Age {
     public static void main(String[] args) {
-        final int AGE =20;
+        final int Age =20;
         int inputAge ;
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter your age : ");
