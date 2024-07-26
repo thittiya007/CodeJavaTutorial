96e0c2abcc1e044faa9b712a88322b450fb6f6b9
thittiya007 <152352137+thittiya007@users.noreply.github.com>
2024-07-26 14:49:45 +0700

    Create Age.java

---
+++ Age.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Age {
+    public static void main(String[] args) {
+        final int AGE =20;
+        int inputAge ;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your age : ");
+        inputAge = scan.nextInt();
+        System.err.println("your entered : "+inputAge);
+        if(inputAge < 20)
+            System.err.println("your are still young");
+        System.err.println("Good Bye");
+    }
+}
