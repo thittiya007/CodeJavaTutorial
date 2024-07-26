804bf2b77b791eafec713b8f7b442d4cebd5b384
thittiya007 <152352137+thittiya007@users.noreply.github.com>
2024-07-26 15:00:57 +0700

    Update StringLength.java

---
+++ StringLength.java
@@ -0,0 +1,8 @@
+public class StringLength {
+    public static void main(String[] args) {
+        String name = "computer";
+        int StringSize;
+        StringSize = name.length();
+        System.out.println(name+" has "+ StringSize +" cheracters ");
+    }
+}
