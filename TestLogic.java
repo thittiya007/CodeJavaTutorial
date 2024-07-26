912d40647ee917c6c65451202aa0fa5c739ccead
thittiya007 <152352137+thittiya007@users.noreply.github.com>
2024-07-26 15:01:00 +0700

    Update TestLogic.java

---
+++ TestLogic.java
@@ -0,0 +1,14 @@
+public class TestLogic {
+    public static void main(String[] args) {
+         int x =6,y=9;
+         System.out.println(x > y);
+         System.out.println(x > y);
+         System.out.println(x >= y);
+         System.out.println(x == y);
+         System.out.println(x != y);
+         System.out.println((x < y) && (y > 3));
+         System.out.println((x == 5) || (y > 2));
+         System.out.println(!(y > x));
+         
+    }
+    }
