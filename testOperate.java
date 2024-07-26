26a03430c8cb558b1fb7efbe822229b32447dfa8
thittiya007 <152352137+thittiya007@users.noreply.github.com>
2024-07-26 15:01:03 +0700

    Update testOperate.java

--- testOperate.java
+++ testOperate.java
@@ -3,8 +3,14 @@ public class testOperate {
     int x, y;
     x = 5 + 2 * 4;
     y = (5 + 2) *4;
+    float value;
+    value = 1;
     System.out.println("x ="+x);
     System.out.println("y ="+y);
+    x = x / 3;
+    System.out.println("x ="+x);
+    value = (float) 4 / 3 ;
+    System.out.println("value ="+value);
    }
 
 }
