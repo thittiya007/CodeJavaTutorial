16e3cf0197be0a8eb138249de6d51a794b5e6023
thittiya007 <152352137+thittiya007@users.noreply.github.com>
2024-07-26 15:00:54 +0700

    Update stringDemo.java

---
+++ stringDemo.java
@@ -0,0 +1,8 @@
+public class stringDemo {
+    public static void main(String[] args) {
+        System greeting ;
+        greeting = "Good morning";
+        String name = "Computer";
+        System.out.println(greeting + " " +name);
+    }
+}
