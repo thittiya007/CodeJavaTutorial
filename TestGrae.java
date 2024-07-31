46622b3ac7e00d0da459f98ebf6b763b059f66a1
thittiya007 <152352137+thittiya007@users.noreply.github.com>
2024-07-31 14:59:11 +0700

    Create TestGrae.java

---
+++ TestGrae.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class TestGrae {
+    public static void main(String[] args) {
+        int score;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your score : ");
+        score = in.nextInt();
+        if (score < 50){
+            System.out.println("Your grade is F : ");
+        }
+        else if (score < 60);{
+            System.out.println("Your grade is D : ");
+        }
+        else if (score < 70);{
+            System.out.println("Your grade is C : ");
+        }
+        else if (score < 70);{
+            System.out.println("Your grade is B : ");
+        }
+        else if (score < 80);{
+            System.out.println("Your grade is A : ");
+        } 
+        else  (score < 70);{
+            System.out.println("Invalid score. ");
+        }
+    }
+}
