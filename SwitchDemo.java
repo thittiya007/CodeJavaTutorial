01c736c8a83bbc4621f1c7d8c32193112f238f7b
thittiya007 <152352137+thittiya007@users.noreply.github.com>
2024-07-31 15:25:36 +0700

    Update SwitchDemo.java

--- SwitchDemo.java
+++ SwitchDemo.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
-
 public class SwitchDemo {
     public static void main(String[] args) {
         int number;
         Scanner keyboard = new Scanner(System.in);
-        System.out.println( "Enter 1,2,or 3 :");
+        System.out.print("Enter 1,2, or 3 : ");
         number = keyboard.nextInt();
-        switch (number) {
-            case 1:
-            System.out.println("You enter 1.");    
+        switch(number){
+            case 1 :
+            System.out.println("You enter 1.");
             break;
             case 2 :
             System.out.println("You enter 2.");
-            default:
+            break;
             case 3 :
             System.out.println("You enter 3.");
+            break;
             default :
-            System.out.println("that's not 1,2, or or 3!");
+            System.out.println("that's not 1,2, or 3!");
+            break;
         }
-
     }
 }
