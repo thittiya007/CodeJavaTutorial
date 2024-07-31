5d18d668fa6a69c5c92367e8d3421781e393f8f8
thittiya007 <152352137+thittiya007@users.noreply.github.com>
2024-07-31 14:59:04 +0700

    Create MinOfThree.java

---
+++ MinOfThree.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MinOfThree {
+    public static void main(String[] args) {
+        int num1,num2,num3,min = 0;
+        Scanner in = new Scanner|(System.in);
+        System.out.println("Enter your three integer");
+        num1 = in.nextInt();
+        num2 = in.nextInt();
+        num3 = in.nextInt();
+        if(num1 < num3){
+            min = num3;
+        }
+    }else{
+    
+    }
+
+        
+    }
+}
