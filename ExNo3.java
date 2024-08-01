944fa24346bec7de9d22224b1f7e078c481f66c4
thittiya007 <152352137+thittiya007@users.noreply.github.com>
2024-08-01 14:32:36 +0700

    Update ExNo3.java

---
+++ ExNo3.java
@@ -0,0 +1,9 @@
+public class ExNo3 {
+    public static void main(String[] args) {
+        int result;
+        for (int i = 1; i <= 12; i++) {
+            result = 8 * i;
+            System.out.println("8 X " + i + " = " +result);
+        }
+    }
+}
