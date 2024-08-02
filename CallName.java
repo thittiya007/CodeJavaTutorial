c43f1f3f35b97512fa6ee8dc5571112cf0504aa2
thittiya007 <152352137+thittiya007@users.noreply.github.com>
2024-08-02 10:14:55 +0700

    Create CallName.java

---
+++ CallName.java
@@ -0,0 +1,12 @@
+public class CallName {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++){
+            CallName();
+        }
+        System.out.println("Back in main");
+    }
+    public static void CallName() {
+        System.out.println();
+        System.out.println("Duangkamon Nammunnoi");
+    }
+}
