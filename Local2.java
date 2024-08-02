8236ad137360325d6ba59c6b24f60f16b728cf00
thittiya007 <152352137+thittiya007@users.noreply.github.com>
2024-08-02 15:19:37 +0700

    Create Local2.java

---
+++ Local2.java
@@ -0,0 +1,13 @@
+public class Local2 {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println("x = "+x);
+        one();
+        x = 10;
+        System.out.println("x = "+x);
+    }
+    public static void one() {
+        int x = 3;
+        System.out.println("x = "+x);
+    }
+}
