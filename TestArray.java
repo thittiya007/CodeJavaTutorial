2ed82fd8a9724b124fc8bc813418670ef80b579d
thittiya007 <152352137+thittiya007@users.noreply.github.com>
2024-08-07 13:56:08 +0700

    Create TestArray.java

---
+++ TestArray.java
@@ -0,0 +1,24 @@
+public class TestArray {
+    public static void main(String[] args) {
+        int [] num = new int [8];
+        num[0] = 2;
+        num[1] = 4;
+        num[2] = 6;
+        num[3] = 8;
+        String [] s = new String[2];
+        s[0] = "math";
+        s[1] = "Psysics";
+        System.out.println("num[0] = : "+num[0]);
+        System.out.println("num[0] = : "+num[1]);
+        System.out.println("num[0] = : "+num[2]);
+        System.out.println("num[0] = : "+num[3]);
+        System.out.println("num[0] = : "+num[4]);
+        num[3] = num[1+1] + num[0];
+        System.out.println("num[3] = : "+num[3]);
+        System.out.println("Length = : "+num.length);
+        System.out.println("s[0] = :"+s[0]);
+        System.out.println("s[1] = :"+s[1]);
+        System.out.println("s[2] = :"+s[2]);
+        //System.out.println("s[2] = :"+s[2]); error : Array IndexOutofBoundsException เกิด error เมื่อมีการแสดงค่าตัวแปลที่ยังไม่ได้ใส่ค่าหรือยังไม่มีค่า
+    }
+}
