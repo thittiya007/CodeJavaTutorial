575dbebc65bd51e3f643350ff4a2b7cdc92c1f54
thittiya007 <152352137+thittiya007@users.noreply.github.com>
2024-08-07 13:55:32 +0700

    Create BasicArra.java

---
+++ BasicArra.java
@@ -0,0 +1,13 @@
+public class BasicArray {
+    public static void main(String[] args) {
+        final int LIMIT = 15, MULTIPLE = 10;
+        int [] list = new int[LIMIT];
+        for(int index = 0; index < LIMIT; index++){
+            list[index] = index * MULTIPLE;
+        }
+        list[5] = 999;
+        for(int value : list){
+            System.out.println(value+" ");
+        }
+    }
+}
